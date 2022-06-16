33302ad7fd0f1cf725ed5cefa17df963eb67c644
ssalgadoi <se.salgado@duocuc.cl>
2022-06-15 23:56:35 -0400

    avance api clima

--- met_office/clima_api/urls.py
+++ met_office/clima_api/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 
 
 router = DefaultRouter()
-router.register('descripcion', views.DescripcionViewSet)
+router.register('clima', views.DescripcionViewSet)
 
 urlpatterns = [
     path('',include(router))
